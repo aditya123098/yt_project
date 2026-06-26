214de44d68623a4c31b37b250ab5217e589cf52f
aditya thakur <[valid.email]>
2026-06-26 17:22:42 +0530

    fix: update langchain imports to use langchain_text_splitters package

--- rag_pipeline.py
+++ rag_pipeline.py
@@ -10,7 +10,7 @@ import logging
 from typing import Optional
 
 import streamlit as st
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import FAISS
 from langchain_huggingface import HuggingFaceEmbeddings
 from youtube_transcript_api import YouTubeTranscriptApi
